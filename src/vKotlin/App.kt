a78528c25ad3e919ba791fe13f1e1c5a4161c2f1
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2020-04-05 14:09:13 -0300

    v3

--- src/vKotlin/App.kt
+++ src/vKotlin/App.kt
@@ -1,24 +1,52 @@
 package vKotlin
 
 import java.io.File
+val data = mutableListOf<Interval>()
 
 fun main() {
     val initialTime = System.currentTimeMillis()
-    val filter = Filter(mutableListOf())
+    var linesRead = 0
 
-    File("src/vKotlin/resource/cohen01.txt")
+    File("src/vKotlin/resource/cohen11.txt")
         .forEachLine {
             val lineValues = it.split("-".toRegex())
             val interval = Interval(lineValues[0].toInt(), lineValues[1].toInt())
-            filter.add(interval)
+            data.add(interval)
+            linesRead++
         }
-    filter.organize()
 
-    println("Tamanho da lista " + filter.getSize())
+    data.sort()
 
+    for(i in 1..data.size) {
+        merger(data.removeAt(0))
+    }
+
+    println("Tamanho da lista " + data.size)
+    println("Total de linhas lidas $linesRead")
     val endTime = System.currentTimeMillis()
     val executionTime = endTime - initialTime
     println("\n\nTempo de execução: $executionTime ms")
+}
 
+fun merger(interval: Interval) {
+    for(i : Interval in data) {
+        if(interval.firstValue <= i.firstValue && interval.lastValue >= i.firstValue
+            && interval.lastValue <= i.lastValue) {
+            i.mergeFirst(interval)
+            return
+        }
 
+        if(interval.firstValue <= i.lastValue && interval.firstValue >= i.firstValue
+            && interval.lastValue >= i.lastValue) {
+            i.mergeLast(interval)
+            return
+        }
+
+        if(interval.firstValue <= i.firstValue && interval.lastValue >= i.lastValue){
+            i.mergeFirst(interval)
+            i.mergeLast(interval)
+            return
+        }
+    }
+    data.add(interval)
 }
