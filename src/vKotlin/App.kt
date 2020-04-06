f2cf7b024b49a48c7bb19445e9f9e30c6dcb2ffd
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2020-04-06 15:58:24 -0300

    V4 - retira comentários e ajusta noem de variáveis

--- src/vKotlin/App.kt
+++ src/vKotlin/App.kt
@@ -20,30 +20,27 @@ fun main() {
     val data = mutableListOf<Interval>()
     val finalData = mutableListOf<Interval>()
 
-    File("src/vKotlin/resource/cohen12.txt")  //leitura do arquivo para adicionar ao data
+    File("src/vKotlin/resource/cohen12.txt")
         .forEachLine {
             val lineValues = it.split("-".toRegex())
             val interval = Interval(lineValues[0].toInt(), lineValues[1].toInt())
             data.add(interval)
         }
-
     data.sort()
+    println("Tamanho da lista inicial " + data.size)
 
-    var currentInterval = data[0]
-    finalData.add(currentInterval) //adiciona o primeiro intervalo (menor first value)
+    var currentInterval = data.removeAt(0)
+    finalData.add(currentInterval)
 
     for (nextInterval in data) {
         if (currentInterval.lastValue >= nextInterval.firstValue) {
-            //compara se o lastValue do primeiro intervalo é maior ou igual
-            // que o first value do próximo
-            currentInterval.merge(nextInterval) //faz o merge do intervalo
+            currentInterval.merge(nextInterval)
         } else {
-            currentInterval = nextInterval //currentInterval recebe a referencia do próximo intervalo
-            finalData.add(currentInterval) //adiciona o currentInterval no finalData
+            currentInterval = nextInterval
+            finalData.add(currentInterval)
         }
     }
 
-    println("Tamanho da lista inicial " + data.size)
     println("Tamanho da lista final " + finalData.size)
     val endTime = System.currentTimeMillis()
     val executionTime = endTime - initialTime
