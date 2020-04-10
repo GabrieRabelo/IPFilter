30d65a0ef8a1b47224fe0d97623278607e2a8351
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2020-04-10 18:50:30 -0300

    Segunda solucao

--- src/vKotlin/App.kt
+++ src/vKotlin/App.kt
@@ -22,3 +22,71 @@ fun main() {
 
 
 }
+
+class Filter(private var data: MutableList<Interval>) {
+    fun add(interval: Interval) {
+        if(data.isEmpty()) {
+            data.add(interval)
+            return
+        }
+        checkValues(interval)
+    }
+
+    fun organize() {
+        val tamanho = data.size
+        for(i in 1..tamanho) {
+            checkValues(data.removeFirst())
+        }
+    }
+
+    private fun checkValues(inputInterval: Interval) {
+        for(i : Interval in data) {
+            if(inputInterval.firstValue <= i.firstValue && inputInterval.lastValue >= i.firstValue
+                && inputInterval.lastValue <= i.lastValue) {
+                i.mergeFirst(inputInterval)
+                return
+            }
+
+            if(inputInterval.firstValue <= i.lastValue && inputInterval.firstValue >= i.firstValue
+                && inputInterval.lastValue >= i.lastValue) {
+                i.mergeLast(inputInterval)
+                return
+            }
+
+            if(inputInterval.firstValue <= i.firstValue && inputInterval.lastValue >= i.lastValue){
+                i.mergeFirst(inputInterval)
+                i.mergeLast(inputInterval)
+                return
+            }
+        }
+        data.add(inputInterval)
+    }
+
+    fun getSize():Int {
+        return data.size
+    }
+
+    override fun toString(): String {
+        return data.toString();
+    }
+}
+
+class Interval (var firstValue: Int, var lastValue: Int): Comparable<Interval> {
+    override fun compareTo(other: Interval): Int {
+        return if (this.firstValue != other.firstValue) {
+            this.firstValue - other.firstValue
+        } else 0
+    }
+
+    fun mergeFirst(interval: Interval) {
+        this.firstValue = interval.firstValue
+    }
+
+    fun mergeLast(interval: Interval) {
+        this.lastValue = interval.lastValue
+    }
+
+    override fun toString(): String {
+        return "$firstValue - $lastValue"
+    }
+}
